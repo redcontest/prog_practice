59005810d36b15965b248d0cd2e8d0f98c370690
redcontest <thezharklee@gmail.com>
2025-09-27 14:13:25 +0500

    Create prac_1_3.py
    практика 1, задача 4

---
+++ prac_1_3.py
@@ -0,0 +1,13 @@
+# задача 4
+s = input('Введите текст: ').upper()
+count = []
+symbols = set(s)
+
+for c in symbols:
+    count.append([c, s.count(c)])
+    print(f'{c} встречается {s.count(c)} раз')
+  
+count.sort(key=lambda x: x[1], reverse=True)
+print('3 самых частых символа:')
+for i in range(3):
+    print(f'{count[i][0]} (встречается {count[i][1]} раз)')
