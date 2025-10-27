ded41342548d91c3e8ac8af6412afc93b6eaa5fc
redcontest <thezharklee@gmail.com>
2025-10-27 13:24:23 +0500

    откат на прошлую версию

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -2,11 +2,8 @@
 
 from gameparts import Board
 
-# Всё, что ниже этой инструкции, не будет импортироваться,
-# но будет выполняться при запуске файла game.py.
-if __name__ == '__main__':
-    game = Board()
-    game.display()
-    game.make_move(1, 1, 'X')
-    print('Ход сделан!')
-    game.display()
+game = Board()
+game.display()
+game.make_move(1, 1, 'X')
+print('Ход сделан!')
+game.display()
